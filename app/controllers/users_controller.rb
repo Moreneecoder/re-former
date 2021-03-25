6a03f89f6285f800dac4bfbe2e854fb1a3e4cc38
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-26 00:08:14 +0100

    Add edit and update views and actions. Add validation display to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,31 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-    if @user.save
+
+    if @user.valid?
+      @user.save
       redirect_to new_user_path
     else
       render :new
     end
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.valid?
+      @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      p @user.errors.full_messages
+      render :edit
+    end
   end
 
   private
